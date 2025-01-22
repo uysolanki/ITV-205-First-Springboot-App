efff52bbd19dd75539be5eb18b495dfaf625bf0f
Umar Solanki <uysolanki@gmail.com>
2025-01-22 18:22:13 +0530

    sb205

--- src/main/java/com/itv/ITV205FirstSpringbootApp/controller/CustomerController.java
+++ src/main/java/com/itv/ITV205FirstSpringbootApp/controller/CustomerController.java
@@ -1,34 +1,82 @@
 package com.itv.ITV205FirstSpringbootApp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itv.ITV205FirstSpringbootApp.model.Customer;
 import com.itv.ITV205FirstSpringbootApp.service.CustomerService;
 
 @RestController
+@RequestMapping("/blinkit/v1")
 public class CustomerController {
 
 	@Autowired
 	CustomerService customerService;
-	
+
 	@RequestMapping("/first")
-	public String welcome()
-	{
+	public String welcome() {
 		return "hello";
 	}
-	
-	@RequestMapping("/addCustomer")
-	public String addCustomer()
-	{
-		Customer c=new Customer();
+
+	@PostMapping("/addCustomer")
+	public String addCustomer() {
+		Customer c = new Customer();
 		c.setCustEmail("alice@gmail.com");
 		c.setCustGender("f");
 		c.setCustName("Alice");
-		
 		customerService.addCustomer(c);
 		return "Customer Added Successfully";
+	}
+
+	@PostMapping("/addCustomerByRequestParam")
+	public String addCustomerByRequestParam(@RequestParam("a") String email, @RequestParam("b") String gen,
+			@RequestParam("c") String name) {
+		Customer c = new Customer();
+		c.setCustEmail(email);
+		c.setCustGender(gen);
+		c.setCustName(name);
+		customerService.addCustomer(c);
+		return "Customer Added Successfully";
+	}
+
+	@PostMapping("/addCustomerByRequestBody")
+	public Customer addCustomerByRequestBody(@RequestBody Customer customer) {
+		return customerService.addCustomer(customer);
+	}
+	
+	@PostMapping("/addMultipleCustomers")
+	public List<Customer> addMultipleCustomers(@RequestBody List<Customer> customers) {
+		return customerService.addMultipleCustomers(customers);
+	}
+	
+	@GetMapping("/getAllCustomers")
+	public List<Customer> getAllCustomers() {
+		return customerService.getAllCustomers();
+	}
+	
+	@GetMapping("/getCustomer/{custid}")
+	public Customer getCustomer(@PathVariable("custid") int cid) {
+		return customerService.getCustomer(cid);
+	}
+	
+	@PutMapping("/updateCustomer/{custid}")
+	public Customer updateCustomer(@PathVariable("custid") int cid,@RequestBody Customer newValues) {
+		return customerService.updateCustomer(cid,newValues);
+	}
+	
+	@DeleteMapping("/deleteCustomer/{custid}")
+	public String deleteCustomer(@PathVariable("custid") int cid) {
+		customerService.deleteCustomer(cid);
+		return "Record Deleted Successfully";
 	}
 }
--- src/main/java/com/itv/ITV205FirstSpringbootApp/service/CustomerService.java
+++ src/main/java/com/itv/ITV205FirstSpringbootApp/service/CustomerService.java
@@ -1,5 +1,8 @@
 package com.itv.ITV205FirstSpringbootApp.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,34 @@ public class CustomerService {
 
 	@Autowired
 	CustomerRespository customerRespository;
-	public void addCustomer(Customer c) {
-		customerRespository.save(c);
+	public Customer addCustomer(Customer c) {
+		return customerRespository.save(c);
+		
+	}
+	public List<Customer> getAllCustomers() {
+		return customerRespository.findAll();
+	}
+	
+	public Customer getCustomer(int cid) {
+		Optional<Customer> optCustomer=customerRespository.findById(cid);
+		Customer customer=null;
+		if(optCustomer.isPresent())
+		{
+			customer=optCustomer.get();
+		}
+		return customer;
+	}
+	public List<Customer> addMultipleCustomers(List<Customer> customers) {
+		return customerRespository.saveAll(customers);
+	}
+	public Customer updateCustomer(int cid, Customer newValues) {
+		Customer dbValues=getCustomer(cid);
+		dbValues.setCustEmail(newValues.getCustEmail());
+		dbValues.setCustName(newValues.getCustName());
+		return customerRespository.save(dbValues);
+	}
+	public void deleteCustomer(int cid) {
+		customerRespository.deleteById(cid);
 		
 	}
 
